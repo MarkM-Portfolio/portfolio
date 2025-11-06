e17067956607ac9a8ed5f501bbe79fe114af2e6f
Mark Mon Monteros <monteros.markmon@gmail.com>
2025-11-06 20:27:32 +0800

    push

--- worker.js
+++ worker.js
@@ -1,38 +1,16 @@
-import { getAssetFromKV } from "@cloudflare/kv-asset-handler";
-
 export default {
   async fetch(request, env, ctx) {
-    const url = new URL(request.url);
-
     try {
-      // If user visits "/", try both index.html and hashed versions
+      const url = new URL(request.url);
+
+      // If root, serve index.html
       if (url.pathname === "/" || url.pathname.endsWith("/")) {
-        try {
-          // Try normal /index.html
-          const asset = await getAssetFromKV(
-            { request: new Request(url.origin + "/index.html", request), waitUntil: ctx.waitUntil }
-          );
-          return asset;
-        } catch (err) {
-          // Fallback to hashed version (like index.2234a815c9.html)
-          const manifest = JSON.parse(env.__STATIC_CONTENT_MANIFEST);
-          const hashedKey = Object.keys(manifest).find(k =>
-            k.startsWith("index.") && k.endsWith(".html")
-          );
-          if (hashedKey) {
-            const asset = await getAssetFromKV(
-              { request: new Request(url.origin + "/" + hashedKey, request), waitUntil: ctx.waitUntil }
-            );
-            return asset;
-          }
-          throw err;
-        }
+        url.pathname += "index.html";
       }
 
-      // Normal asset serving
-      const asset = await getAssetFromKV({ request, waitUntil: ctx.waitUntil });
+      // Serve from ASSETS binding
+      const asset = await env.ASSETS.fetch(new Request(url.toString(), request));
       return asset;
-
     } catch (err) {
       return new Response("404 Not Found", { status: 404 });
     }
